f9ec831da9de407999fba883d2d802a4dc433775
SruthiSasidharan <sruthisasidharan15@gmail.com>
2026-01-10 15:51:45 +0530

    build sh

--- core/settings.py
+++ core/settings.py
@@ -197,6 +197,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "https://user-management-system-web.onrender.com"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
