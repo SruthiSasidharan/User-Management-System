789f9ee426c5f8a4ed635f417afbeb6c93a79141
SruthiSasidharan <sruthisasidharan15@gmail.com>
2026-01-12 20:33:39 +0530

    allowed hosts

--- core/settings.py
+++ core/settings.py
@@ -190,6 +190,7 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000",
+    "https://sruthi.pythonanywhere.com/"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,6 @@
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+//const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'https://sruthi.pythonanywhere.com/api';
+
 
 //const API_BASE_URL = 'https://user-management-system-web.onrender.com/api';
 let accessToken = localStorage.getItem('accessToken');
