012b7eda96f0e2c083ca2e21471c404164e97fa2
SruthiSasidharan <sruthisasidharan15@gmail.com>
2026-01-12 20:28:46 +0530

    db changed to sqllite

--- core/settings.py
+++ core/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [str(BASE_DIR / 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
